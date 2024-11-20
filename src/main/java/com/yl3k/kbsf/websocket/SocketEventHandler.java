5c99c38e98403d152e57f28f130501454e062365
yoondabin <yundabin0608@gmail.com>
2024-11-20 23:21:45 +0900

    feat: 웹소켓 로직 변경

--- src/main/java/com/yl3k/kbsf/websocket/SocketEventHandler.java
+++ src/main/java/com/yl3k/kbsf/websocket/SocketEventHandler.java
@@ -2,6 +2,7 @@ package com.yl3k.kbsf.websocket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -31,13 +32,12 @@ public class SocketEventHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
         String payload = message.getPayload();
         SocketMessage msgData = SocketMessageUtils.deserialize(payload);
-
         String type = msgData.getType();
         String roomId = getRoomId(msgData);
 
         switch (type) {
             case "join_room":
-                handleJoinRoom(session, roomId);
+                handleJoinRoom(session, msgData, roomId);
                 break;
             case "offer":
                 handleOffer(session, msgData, roomId);
@@ -62,9 +62,12 @@ public class SocketEventHandler extends TextWebSocketHandler {
         return customerId + "_" + counselorId;
     }
 
-    private void handleJoinRoom(WebSocketSession session, String roomId) throws IOException {
+    private void handleJoinRoom(WebSocketSession session, SocketMessage msgData, String roomId) throws IOException {
         if (socketRoom.containsValue(roomId)) {
             socketRoom.put(session.getId(), roomId);
+            // 방에 있는 다른 사용자가 있다면 통신을 보내게됨
+            msgData.setType("new_user");
+            sendToRoomExcludeSelf(roomId, msgData, session.getId());
             logger.info("User {} joined room {}", session.getId(), roomId);
         } else {
             socketRoom.put(session.getId(), roomId);
@@ -73,15 +76,15 @@ public class SocketEventHandler extends TextWebSocketHandler {
     }
 
     private void handleOffer(WebSocketSession session, SocketMessage msgData, String roomId) {
-        sendToRoom(roomId,  msgData);
+        sendToRoomExcludeSelf(roomId,  msgData, session.getId());
     }
 
     private void handleAnswer(WebSocketSession session, SocketMessage msgData, String roomId) {
-        sendToRoom(roomId, msgData);
+        sendToRoomExcludeSelf(roomId,  msgData, session.getId());
     }
 
     private void handleCandidate(WebSocketSession session, SocketMessage msgData, String roomId) {
-        sendToRoom(roomId, msgData);
+        sendToRoomExcludeSelf(roomId,  msgData, session.getId());
     }
 
     private void handleEndCall(WebSocketSession session, String roomId) {
@@ -113,6 +116,24 @@ public class SocketEventHandler extends TextWebSocketHandler {
             }
         });
     }
+
+    private void sendToRoomExcludeSelf(String roomId, SocketMessage msgData, String myId) {
+        socketRoom.forEach((sessionId, currentRoomId) -> {
+            if (currentRoomId.equals(roomId) && !sessionId.equals(myId)) {
+                WebSocketSession session = users.get(sessionId);
+                if (session != null && session.isOpen()) {
+                    try {
+                        TextMessage msg = SocketMessageUtils.convertSocketMesageToWebSocketMessage(msgData);
+                        session.sendMessage(msg);
+                        logger.info("{} Sent message to user {} in room {}: {}", myId, sessionId, roomId, msgData.getType());
+                    } catch (IOException e) {
+                        logger.error("Error sending message to {}: {}", sessionId, e.getMessage());
+                    }
+                }
+            }
+        });
+    }
+
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
